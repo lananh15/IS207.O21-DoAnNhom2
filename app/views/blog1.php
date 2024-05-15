46380d043c0f9021acdf03bac84efbbfacfea5e1
Ngọc Nguyễn Thị Kim <22520959@ms.uit.edu.vn>
2024-05-16 00:43:02 +0700

    kim ngoc update front-end blog1

---
+++ app/views/blog1.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        integrity="sha512-...your-integrity-hash-here..." crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" type="text/css" href="../../public/css/normalize.css" />
+    <link rel="stylesheet" 
+      href= "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+    <link rel="stylesheet" type="text/css" href="../../public/css/header.css" />
+    <link rel="stylesheet" type="text/css" href="../../public/css/blog1.css" />
+    <title>Blog</title>
+    <link rel="icon" type="image/x-icon"
+</head>
+<body>
+    <?php require_once "header.php" ?>
+    <div id="banner">
+        <h2>Feature</h2>
+        <h1>Secondary emotions concept in</h1>
+        <h1>Inside out 2</h1>
+        <h3 >Published on April <span>16<sup>th</sup></span>, 2024</h3>
+    </div>
+    <div id="Post1">
+        <h2 id="firsth2">This is me clowning, also breaking my silence.</h2>
+        <p id="first">I’ve seen people having different opinions on Inside Out 2; that sequel is like this, sequel is like that. Most of the time, we see how people always ask why Anxiety is an emotion when we already have Fear, or why we don’t see adults have those emotions.</p>
+        <p id="second">And please note that I’m not here to answer those questions; I’ll just let the movie do it itself.</p>
+        <p id="third">As an Inside Out enjoyer, I would like to share some thoughts and theories on the sequel since I find it pretty intriguing.</p>
+        <h2 class="heading">Now, let’s compare the posters.</h2>
+        <center><img src="../../public/images&videos/Blog1/poster-comparing.png" class="banner"></center>
+        <div id="next">
+            <p class="next1">As we can see, there are many small changes to the environment in the poster. The background in the 1st movie is filled with colors of the 5 emotions, while in the 2nd movie, it’s blank, like a white space. Joy’s expressions are different too; in the new poster, she’s just surprised as if she’s trying to process something. “Big changes. New emotions” because puberty hits Riley, and I think adding new emotions to this phase is a nice idea.</p>
+            <p class="next1">It can be a bit complicated to understand since there are different emotional theories</p>
+            <p class="next1">So there are terms called "Primary Emotions" and "Secondary Emotions."</p>
+            <p class="next1">Primary emotions are direct emotional reactions to a situation, and they are called primary because they come first. It is your very first reaction to a situation or event, and they alert you about your needs.</p>
+            <p class="next1">Secondary emotions are the emotions that are often felt after the primary emotion has been experienced. They are the reactions to our primary emotions and are often habitual or learned responses.</p>
+            <p class="next1">The purpose of secondary emotions is to cover up the sensitive primary emotions with something less sensitive. In this way, they are a way of protecting the self from being vulnerable.</p>
+            <p class="next1">Below are some other main differences between primary and secondary emotions.</p>
+       </div>
+        <h2 class="heading">Primary emotions</h2>
+        <p class="primary-emotions">+ Are learned or habitual</p>
+        <p class="primary-emotions">+ Functional</p> 
+        <p class="primary-emotions">+ Can be painful or pleasurable</p>
+        <p class="primary-emotions">+ Can be harmful when reacted to</p>
+        <p class="primary-emotions">+ Are sensitive and vulnerable</p>
+        <p class="primary-emotions">+ Are rooted in the deeper parts of the brain</p>
+        <p class="primary-emotions">+ Can help keep us connected with others</p>
+        <p class="primary-emotions">+ Can help guide our actions</p>
+        <p class="primary-emotions">+ Are instinctive and natural</p>
+        <h2 class="heading">Secondary emotions</h2>
+        <p class="secondary-emotions">+ Are learned or habitual</p>
+        <p class="secondary-emotions">+ Protective</p>
+        <p class="secondary-emotions">+ Defensive and avoidant</p>
+        <p class="secondary-emotions">+ Can be better controlled than primary emotions</p>
+        <p class="secondary-emotions">+ Can numb emotions</p>
+        <p class="secondary-emotions">+ Do not listen to what the emotions are asking</p>
+        <p class="secondary-emotions">+ Comes from a place of learning, e.g., ‘I should not feel sad’ or ‘I should not</p>
+        <p class="secondary-emotions">feel any anger.’ This is not the same as self-control.</p>
+        <p class="secondary-emotions">+ Are motivated by pain reduction</p>
+        <p class="secondary-emotions">+ Linger around long after the event has happened</p>
+        <p class="secondary-emotions">+ Generally, leads to distance and disconnect from our goals, values, and people</p>
+        <p class="next2">In the poster, the four emotions appear underneath the five core emotions. So according to the Secondary Emotions Theory for Inside Out 2, Anxiety, Ennui, Embarrassment, and Envy can be those "secondary emotions," as they will ‘help’ Riley react to the emotions she feels or protect her from feeling ‘vulnerable.’</p>
+        <p class="next2">Plus, Anxiety and the rest seem to just "move in," but not born from the dark like Joy was. Where were they all the time?</p>
+        <p class="next2" id="before">Before puberty hits Riley, what she felt wasn't too complex for the five basic emotions to handle. But now, she is more aware of herself and the surrounding environment, how she would react to it. She may feel more self-conscious.</p>
+        <p class="next2">Live right and live honestly... Like we have to have something to rely on. I think that often when teenagers reach puberty, they want to be more independent than dependent, like they don't need anyone. How we feel, we can solve it ourselves, so the other four emotions were born to do that. Maybe Riley will go through the same psychological change…</p>
+        <p class="next2">+ Now speaking of Fear and Anxiety, Fear's job is to get the body prepared to fight or flee. While Anxiety's job is reacting to the emotions versus danger in the environment. Anxiety is a stop-reaction to the impulses that fear and other core emotions create inside the body. For instance, Fear mobilizes energy for movement, and Anxiety pushes it back down.</p>
+        <center><img src="../../public/images&videos/Blog1/anxiety.png" class="banner"></center>
+        <br><br>
+        <p class="next3">+ Envy can be secondary emotions to Disgust, Anger, and Sadness. Actually, there may also be Fear, but not much.</p>
+        <p class="next3">+ Embarrassment probably works with the four as well.</p>
+        <p class="next3">+ Ennui can mask Anger, Fear, and Sadness…</p>
+        <br>
+        <p class="next3">Either way, unlike the rest, Joy doesn’t seem to have a secondary emotion in the movie. But we do know that Joy tends to mix herself with the other basic emotions more.</p>
+        <center><img src="../../public/images&videos/Blog1/memory-ball-color.png" class="banner" id="small"></center>
+        <p class="next4">However, in my opinion, if the new emotions become secondary emotions to Joy, then Riley will likely experience mental issues instead…</p>
+        <p class="next4">Now coming back to the poster, the blank space behind the emotions could mean that maybe Riley doesn't really want to feel the primary emotions and the secondary emotions rise up to fill that space for her.</p>
+        <p class="next4">This is a complex topic to talk about and Inside Out 2 may want to carry this out so we can have a simpler look to it. In the 1st movie, it’s more about getting to know the voices inside your head, and embracing Sadness to find happiness again. And it's not easy to make a film that exploits the psychology and physiology of this age group, it's very difficult to portray the relationships in your mind vividly. That just makes me intrigued more of how they will portray it in Inside Out 2.</p>
+        <h2 class="heading">Now some thoughts on the plot…</h2>
+        <p class="finally">During puberty, the new emotions will prevent the core ones from touching the console or they will have some conflicts.</p>
+        <p class="finally">So the plot can go like this, I haven’t thought too much about it so feel free to share more with me! Here goes.</p>
+        <p class="finally">At first, it can go smooth, when Riley seems to have more self-control and the core emotions are okay with the way the new emotions keep her calm through changes in puberty. But then they'll realize that it's actually not good for Riley to repress her emotions and how the four emotions drive her may go in an extreme way. She hits puberty, she experiences lots of things, the four take the lead, the five are okay but then they're not okay, or they're just okay since someone did a better job than them and they could go playing on the beach or something.</p>
+        <p class="finally">Or they just fight from the beginning and until the end then learn how to work together, which I'm not very fond of unless they found a good reason.</p>
+        <p class="finally">That's all for the plot based on the Secondary Emotions concept I can come up with for now!</p>
+        <div id="sign">
+        <p id="actor">By Muoidiemdoanweb</p>
+        <p id="date">Updated on April <span>17<sup>th</sup></span> 2024</p>
+       </div>
+    </div>
+    <br>
+    <hr id="h1">
+    <div id="comment-container">
+        <div id="comment"> <div id="number"> </div> comment</div>
+        <form class="comment-form">
+           <img src="../../public/images&videos/Blog2/avatar.png" alt="Avatar" class="avatar">
+            <input type="text" placeholder="Leave a comment" class="comment-box">
+        </form>  
+    </div>
+    <div class="button-container">
+        <button type="submit" id="Cancel" class="button-blog">Cancel</button>
+        <button type="submit" id="Send" class="button-blog">Send</button>
+    </div>
+    <?php require_once "footer.php" ?>
+</body>
+<script>
+</script>
+</html>
